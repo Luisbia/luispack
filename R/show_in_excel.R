a47bdab4c271316295f738532e43c7c6a82b262c
Luisbia <65022632+Luisbia@users.noreply.github.com>
2022-07-09 23:05:46 +0200

    corr

---
+++ R/show_in_excel.R
@@ -0,0 +1,14 @@
+#'Export to excel a data frame
+#' @param .data
+#'
+#' @return an excel file in a temporary folder
+#' @export
+#'
+#' @examples
+#'
+#' show_in_excel(mtcars)
+show_in_excel <- function(.data){
+  tmp <- paste0(tempfile(), ".xlsx")
+  openxlsx::write.xlsx(.data,tmp)
+  browseURL(tmp)
+}
