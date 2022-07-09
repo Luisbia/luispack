f47b4af269464d77283c0c0e5b3c383b4b5fcae3
Luisbia <65022632+Luisbia@users.noreply.github.com>
2022-07-09 23:46:02 +0200

    new functions

---
+++ R/xlsx_to_csv.R
@@ -0,0 +1,22 @@
+#' A function to convert csv files to parquet files
+#'
+#' @param dir
+#'
+#' @return csv files are read with data.table and converted to parquet with arrow. The filenames are the same but adding a suffix .parquet
+#' @export
+#'
+#' @examples
+#'
+#' csv_to_parquet("where_my_csv_are_stored")
+csv_to_parquet<- function(dir){
+  list_csv <- list.files (path = dir,
+                          pattern = glob2rx("*csv$"),
+                          full.names = TRUE)
+
+  csv_to_parquet <- function(file) {
+    data <- data.table::fread (file)
+    arrow::write_parquet(data,paste0(file,".parquet") )
+  }
+
+  purrr::walk(list_csv, csv_to_parquet)
+}
