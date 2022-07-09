d08445e57fc1091485e69267b98cca50fe7b55b5
Luisbia <65022632+Luisbia@users.noreply.github.com>
2022-07-09 23:24:48 +0200

    readme

---
+++ R/show_DT.R
@@ -0,0 +1,16 @@
+#' Create an HTML interactive table with DT from a data frame
+#'
+#' @param x a data frame
+#'
+#' @return DT table
+#' @export
+#'
+#' @examples
+#'
+#' show_DT(mtcars)
+#'
+show_DT <- function(x){
+  DT::datatable(x, filter = "top", class = "stripe hover", extensions = "Buttons",
+                options = list(  lengthMenu = list(c(20, -1), c("20", "All")),
+                                 pageLength = 20, dom = "Blfrtip", buttons = c("excel"))
+  )}
