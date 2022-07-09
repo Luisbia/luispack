6fa448e0227a28479098cba25e966e747f04d574
Luisbia <65022632+Luisbia@users.noreply.github.com>
2022-07-10 00:18:19 +0200

    theme2

---
+++ R/scale_theme2.R
@@ -0,0 +1,63 @@
+#' Apply Eurostat Theme2 palette to fill aesthetics in ggplot
+#'
+#' @examples
+#'
+#' ggplot2::ggplot(mtcars,aes(cyl,mpg, fill=as.factor(cyl)))+
+#' geom_col()+
+#' scale_fill_theme2()
+#'
+scale_fill_theme2 <- function () {
+  theme2_pal <- c("#af4b91",#theme2
+                  "#466eb4",
+                  "#b9c337",
+                  "#41afaa",
+                  "#b93c46",
+                  "#00a0e1",
+                  "#9b3278", #theme2
+                  "#2d50a0",
+                  "#a0aa37",
+                  "#23969b",
+                  "#aa192d",
+                  "#0087cd",
+                  "#be78aa", #theme2
+                  "#6487c3",
+                  "#c8cd64",
+                  "#73bebe",
+                  "#c86e64",
+                  "#64b4e6")
+
+
+  ggplot2::scale_fill_manual(values = theme2_pal)
+}
+
+#' Apply Eurostat Theme2 palette to colour aesthetics in ggplot
+#'
+#'
+#' @examples
+#' ggpplot2::ggplot(mtcars,aes(cyl,mpg, colour=as.factor(cyl)))+
+#' geom_point(size=3)+
+#'   scale_colour_theme2()
+
+scale_colour_theme2 <- function () {
+  theme2_pal <- c("#af4b91",#theme2
+                  "#466eb4",
+                  "#b9c337",
+                  "#41afaa",
+                  "#b93c46",
+                  "#00a0e1",
+                  "#9b3278", #theme2
+                  "#2d50a0",
+                  "#a0aa37",
+                  "#23969b",
+                  "#aa192d",
+                  "#0087cd",
+                  "#be78aa", #theme2
+                  "#6487c3",
+                  "#c8cd64",
+                  "#73bebe",
+                  "#c86e64",
+                  "#64b4e6")
+
+  ggplot2::scale_colour_manual(values = theme2_pal)
+}
+
