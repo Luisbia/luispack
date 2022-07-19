979e46bd80e1c6a7b5308d0cb8a229283e54c74f
Luisbia <65022632+Luisbia@users.noreply.github.com>
2022-07-19 18:52:19 +0200

    linechart

--- R/regacc_linechart.R
+++ R/regacc_linechart.R
@@ -1,6 +1,29 @@
-library(dplyr)
-library(ggplot2)
-
+#' Create standard line charts
+#'
+#'This functions makes easy to create standard line charts for different purposes
+#' @param dat the data frame with the data
+#' @param hor the name of the variable in the horizontal axis
+#' @param ver the name of the variable in the vertical axis
+#' @param grp the variable to use for grouping the data
+#' @param clr the variable mapped to the colour aesthetic
+#' @param leg set to FALSE if you do not want to show the legend
+#' @param fac the (unique) variable to use if small multiples are wanted
+#'
+#' @return a ggplot
+#' @export regacc_linechart
+#'
+#' @examples
+#' df<- NQR_data %>%
+#' filter(Country =="AT" & vintage =="2021" & NUTS =="2") %>%
+#'   mutate(time=as.integer(time))
+#'
+#' regacc_linechart(dat=df,
+#'                  hor=time,
+#'                  ver=values,
+#'                  grp=geo,
+#'                  clr=geo,
+#'                  leg=TRUE,
+#'                  fac=na_item)
 regacc_linechart <- function(dat,
                              hor,
                              ver,
@@ -13,13 +36,14 @@ regacc_linechart <- function(dat,
   grp <- enquo(grp)
   clr <- enquo(clr)
 
+  check_packages()
   theme_linechart <- function(..., base_size = 14, base_family="mono") {
     theme(
       panel.grid.minor = element_blank(),   ## drop minor gridlines
       panel.grid.major.y =  element_line(color = "#7D8088", linetype = 2,),# change horizontalgrid lines to gray
       panel.grid.major.x =  element_blank(),     # remove vertical gridlines
-      panel.background = element_rect(fill = "#FAFAFA", color = "#262B38"),      # fill the plot and panel spaces with grey and remove border
-      plot.background = element_rect(fill = "#FAFAFA", color = "#262B38"),
+      panel.background = element_rect(fill = "#FFFFFF", color = "#262B38"),      # fill the plot and panel spaces with grey and remove border
+      plot.background = element_rect(fill = "#FFFFFF", color = "#262B38"),
       panel.border = element_blank(),
       legend.title = element_blank(),
       legend.text = element_text( colour ="#262B38",size=rel(1.0)),
@@ -34,14 +58,20 @@ regacc_linechart <- function(dat,
       plot.caption = element_text(size = rel(1.1),hjust = 0.05, vjust = 0, face="italic"),
       plot.caption.position = "plot",
       strip.background = element_rect(fill = "#0E47CB"),
-      strip.text = element_text(size=rel(1.2), face= "bold", colour ="#FFCC00"),
+      strip.text.x = element_text(  size = rel (1.3), face = "bold", hjust = 0.1, colour = "white" ), #margin = margin()
+      strip.text.y = element_text(size = rel (1.3),hjust = 0.1, face= "bold", colour = "white"),
+      strip.placement = "outside",
+      panel.spacing = unit(0.8, "lines"),
       ...)}
 
   plot <- ggplot(dat) +
     geom_line(aes(!!hor, !!ver, group = !!grp, colour = !!clr),size=0.8)+
     theme_linechart()+
-    scale_x_continuous(labels = scales::label_number(), breaks = scales::breaks_pretty(n=3))+
-    scale_y_continuous(labels = scales::label_number(), breaks = scales::breaks_pretty(n=4))
+    scale_x_continuous(labels = scales::label_number(accuracy = 1),
+                       breaks = scales::breaks_pretty(n=3),
+                       expand = c(0,0))+
+    scale_y_continuous(labels = scales::label_number(),
+                       breaks = scales::breaks_pretty(n=4))
 
   if (leg == FALSE) {
     plot <- plot + theme(legend.position = "none")
@@ -57,14 +87,4 @@ regacc_linechart <- function(dat,
   return(plot)
 }
 
-# df<- NQR_data %>%
-#   filter(Country =="AT" & vintage =="2021" & NUTS =="2")
-#
-#
-# regacc_linechart(dat=df,
-#                  hor=time,
-#                  ver=values,
-#                  grp=geo,
-#                  clr=geo,
-#                  leg=TRUE,
-#                  fac=na_item)
+
